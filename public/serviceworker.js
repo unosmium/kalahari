49808e82711a343b784f33781038f2237b34dc4f
Em Zhan <zqianem@gmail.com>
2020-11-17 17:42:28 -0500

    Increment service worker cache

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('kalahri-v1').then(cache => {
+    caches.open('kalahri-v2').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
